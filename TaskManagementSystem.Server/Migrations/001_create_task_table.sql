d7efd2845af0cf57940faad57513c2837c2df4d8
Richard-S-Rodrigues <richardsouzarodrigues555@gmail.com>
2023-08-20 19:36:08 -0300

    Fix migration

--- TaskManagementSystem.Server/Migrations/001_create_task_table.sql
+++ TaskManagementSystem.Server/Migrations/001_create_task_table.sql
@@ -1,9 +1,9 @@
-CREATE TABLE task (
-  id serial primary key,
-  name text not null,
-  description text,
-  status int not null,
-  sub_tasks serial[] not null,
-  created_at timestamp not null,
-  updated_at timestamp not null,
-) 
+CREATE TABLE IF NOT EXISTS "task" (
+  "id" serial primary key,
+  "name" text not null,
+  "description" text,
+  "status" int not null,
+  "sub_tasks" int[] not null,
+  "created_at" timestamp not null,
+  "updated_at" timestamp not null
+); 
