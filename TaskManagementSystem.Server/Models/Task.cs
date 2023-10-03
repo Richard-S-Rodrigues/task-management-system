c3116b10883bc98bd8a69c6d3e57ba843baab53c
Richard-S-Rodrigues <richardsouzarodrigues555@gmail.com>
2023-10-02 21:37:28 -0300

    Fix: get tasks not working

--- TaskManagementSystem.Server/Models/Task.cs
+++ TaskManagementSystem.Server/Models/Task.cs
@@ -6,7 +6,7 @@ public class TaskModel
   public required string Name { get; set; }
   public string? Description { get; set; }
   public long TaskListId { get; set; }
-  public IList<long> SubTaskIds { get; set; } = new List<long>();
+  public IList<int> SubTaskIds { get; set; } = new List<int>();
   public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
   public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 }
